61f9a8e1eb1720268372cdb7dc7d89b958b9b34c
robinwhg <mail@robinwhg.com>
2026-03-25 13:38:29 +0100

    fea: add card images

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,31 +4,37 @@ const playableRegions = [
     slug: 'world',
     title: 'World',
     description: 'Play with all available countries in one run.',
+    img: '/img/world.svg',
   },
   {
     slug: 'africa',
     title: 'Africa',
     description: 'Focus on African countries and territories.',
+    img: '/img/africa.svg',
   },
   {
     slug: 'americas',
     title: 'Americas',
     description: 'North, Central, South America, and the Caribbean.',
+    img: '/img/americas.svg',
   },
   {
     slug: 'asia',
     title: 'Asia',
     description: 'Play countries across Western to Eastern Asia.',
+    img: '/img/asia.svg',
   },
   {
     slug: 'europe',
     title: 'Europe',
     description: 'Challenge yourself with European flags.',
+    img: '/img/europe.svg',
   },
   {
     slug: 'oceania',
     title: 'Oceania',
     description: 'Includes Australia, Polynesia, Micronesia, and Melanesia.',
+    img: '/img/oceania.svg',
   },
 ]
 </script>
@@ -50,7 +56,10 @@ const playableRegions = [
               :description="r.description"
               :to="`/play/${r.slug}`"
               variant="subtle"
-            />
+              class="transition-transform hover:scale-105"
+            >
+              <img v-if="r.img" :src="r.img" class="object-contain object-center max-h-32 w-full">
+            </UPageCard>
           </UPageGrid>
         </template>
       </UPageHero>
